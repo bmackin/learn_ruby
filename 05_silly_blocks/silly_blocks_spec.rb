471f76f8d15e4ef105e42baff2a1da2fd8b76ac1
Matt Vleming <mattvleming@gmail.com>
2013-08-01 18:15:26 -0400

    Adding comments to 05

--- 05_silly_blocks/silly_blocks_spec.rb
+++ 05_silly_blocks/silly_blocks_spec.rb
@@ -1,10 +1,7 @@
-# # Topics
-#
 # * blocks
 # * closures
 # * yield
 # * loops
-#
 
 require "silly_blocks"
 
@@ -16,9 +13,38 @@ describe "some silly block functions" do
         "hello"
       end
       result.should == "olleh"
+      # You might be tempted to ask yourself what the hell is happening here.
+      # Well no worries! Superman is here to the rescue!
+      # (Heh, you probably don't even know whose written these comments.)
+      # 
+      # Let's look at this snippet of code in particular:
+      # 
+      # reverser do 
+      #   "hello"
+      # end
+      #
+      # reverser is still a method, and it should return the reverse of the given string, i.e. "olleh",
+      # except the string is not being passed in as a parameter. It's being passed in as a do-end block.
+      # The way this method can get that do-end block is through yield.
+      #
+      # def example 
+      #   string = yield # yield is equal to... 
+      #   string.upcase
+      # end
+      # 
+      # example do 
+      #   "My name is Will Richman." # ...this line
+      # end
+      # 
+      # So to get this test to pass the reverser method would look like:
+      # 
+      # def reverser
+      #   yield.reverse
+      # end
     end
 
     it "reverses each word in the string returned by the default block" do
+      # Now edit the reverser method above to get this test to pass 
       result = reverser do
         "hello dolly"
       end
@@ -31,6 +57,10 @@ describe "some silly block functions" do
       adder do
         5
       end.should == 6
+      # def adder
+      #   num = yield
+      #   # what do you want do you with num?
+      # end
     end
 
     it "adds 3 to the value returned by the default block" do
@@ -42,11 +72,11 @@ describe "some silly block functions" do
 
   describe "repeater" do
     it "executes the default block" do
-      block_was_executed = false
+      n = 2
       repeater do
-        block_was_executed = true
+        n += 3
       end
-      block_was_executed.should == true
+      n.should == 4
     end
 
     it "executes the default block 3 times" do
