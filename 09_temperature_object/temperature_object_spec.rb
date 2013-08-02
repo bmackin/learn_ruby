30f7a83694196853977ca23a6d20f7cd891918aa
Matt Vleming <mattvleming@gmail.com>
2013-08-02 10:06:40 -0400

    Adding comments to 09

--- 09_temperature_object/temperature_object_spec.rb
+++ 09_temperature_object/temperature_object_spec.rb
@@ -1,21 +1,9 @@
-# # Topics:
-# * floating-point math
-# * objects
-# * constructors
-# * class methods
-# * factory methods
-# * options hashes
-#
-# # Hints
-#
-# Remember that one degree fahrenheit is 5/9 of one degree celsius, and that the freezing point of water is 0 degrees celsius but 32 degrees fahrenheit.
-#
-# Remember to define the `from_celsius` factory method as a *class* method, not an *instance* method.
-#
-# The temperature object's constructor should accept an *options hash* which contains either a `:celcius` entry or a `:fahrenheit` entry.
-#
-#
-#
+# Note: The problems are becoming more difficult now!
+# Take your time with the next four, don't be scared to ask for help,
+# we're pushing your limits, if you can do them you'll be in an amazing position moving forward.
+
+# Remember one degree fahrenheit is 5/9 of one degree celsius, 
+# and the freezing point of water is 0 degrees celsius but 32 degrees fahrenheit.
 
 require "temperature"
 
@@ -24,79 +12,82 @@ describe Temperature do
   describe "can be constructed with an options hash" do
     describe "in degrees fahrenheit" do
       it "at 50 degrees" do
-        Temperature.new(:f => 50).in_fahrenheit.should == 50
+        # A hash with the key "f" is being passed in.
+        Temperature.new({:f => 50}).to_fahrenheit.should == 50
+        # Remember, new is synonymous with initialize.
+        # An example might help:
+        # 
+        # class Person
+        #   def initialize(name)
+        #     @name = name
+        #   end
+        # end
+        # 
+        # Person.new("Alex")
       end
 
       describe "and correctly convert to celsius" do
         it "at freezing" do
-          Temperature.new(:f => 32).in_celsius.should == 0
+          Temperature.new({:f => 32}).to_celsius.should == 0
         end
 
         it "at boiling" do
-          Temperature.new(:f => 212).in_celsius.should == 100
+          Temperature.new({:f => 212}).to_celsius.should == 100
         end
 
         it "at body temperature" do
-          Temperature.new(:f => 98.6).in_celsius.should == 37
+          Temperature.new({:f => 98.6}).to_celsius.should == 37
         end
 
         it "at an arbitrary temperature" do
-          Temperature.new(:f => 68).in_celsius.should == 20
+          Temperature.new({:f => 68}).to_celsius.should == 20
         end
       end
     end
 
     describe "in degrees celsius" do
       it "at 50 degrees" do
-        Temperature.new(:c => 50).in_celsius.should == 50
+        # Now a hash with the key "c" is being passed in.
+        Temperature.new({:c => 50}).to_celsius.should == 50
       end
 
       describe "and correctly convert to fahrenheit" do
         it "at freezing" do
-          Temperature.new(:c => 0).in_fahrenheit.should == 32
+          Temperature.new({:c => 0}).to_fahrenheit.should == 32
         end
 
         it "at boiling" do
-          Temperature.new(:c => 100).in_fahrenheit.should == 212
-        end
-
-        it "at body temperature" do
-          Temperature.new(:c => 37).in_fahrenheit.should be_within(0.1).of(98.6)
-          # Why do we need to use be_within here?
-          # See http://www.ruby-forum.com/topic/169330
-          # and http://groups.google.com/group/rspec/browse_thread/thread/f3ebbe3c313202bb
-          # Also, try "puts 0.5 - 0.4 - 0.1" -- pretty crazy, right?
+          Temperature.new({:c => 100}).to_fahrenheit.should == 212
         end
       end
     end
   end
 
-  # Factory Method is a design pattern, not a Ruby language feature.
-  # One way to implement this pattern in Ruby is via class methods --
-  # that is, methods defined on the class (Temperature) rather than
-  # on individual instances of the class.
   describe "can be constructed via factory methods" do
 
     it "in degrees celsius" do
-      Temperature.from_celsius(50).in_celsius.should == 50
-      Temperature.from_celsius(50).in_fahrenheit.should == 122
+      Temperature.in_celsius(50).to_celsius.should == 50
+      Temperature.in_celsius(50).to_fahrenheit.should == 122
+      # Nothing different is being achieved with these two lines,
+      # they're just a different way of doing the same thing,
+      # that requires you to write your code another way. 
+      #
+      # Remember, methods called on a class are called class methods.
+      # Here's an example:
+      #
+      # class Person
+      #   def self.what_am_i
+      #     return "I am a person."
+      #   end
+      # end
+      #
+      # Person.what_am_i # => "I am a person."
     end
 
     it "in degrees fahrenheit" do
-      Temperature.from_fahrenheit(50).in_fahrenheit.should == 50
-      Temperature.from_fahrenheit(50).in_celsius.should == 10
+      Temperature.in_fahrenheit(50).to_fahrenheit.should == 50
+      Temperature.in_fahrenheit(50).to_celsius.should == 10
     end
-
-  end
-
-  # test-driving bonus:
-  #
-  # 1. make two class methods -- ftoc and ctof
-  # 2. refactor to call those methods from the rest of the object
-  #
-  # run *all* the tests during your refactoring, to make sure you did it right
-  #
-  describe "utility class methods" do
 
   end
 
@@ -104,25 +95,42 @@ describe Temperature do
   describe "Temperature subclasses" do
     describe "Celsius subclass" do
       it "is constructed in degrees celsius" do
-        Celsius.new(50).in_celsius.should == 50
-        Celsius.new(50).in_fahrenheit.should == 122
+        Celsius.new(50).to_celsius.should == 50
+        Celsius.new(50).to_fahrenheit.should == 122
+        # Again, this is a different way of doing the same thing.
+        # Instead of initializing a new Temperature with a hash that has a key "c",
+        # we're just initializing a new Celsius.
       end
 
       it "is a Temperature subclass" do
+        # In other words, Celsius *inherits* from Temperature.
         Celsius.new(0).should be_a(Temperature)
+        # Remember class inheritance?
+        #
+        # class Person < Mammal
+        # end
       end
     end
 
     describe "Fahrenheit subclass" do
       it "is constructed in degrees fahrenheit" do
-        Fahrenheit.new(50).in_fahrenheit.should == 50
-        Fahrenheit.new(50).in_celsius.should == 10
+        Fahrenheit.new(50).to_fahrenheit.should == 50
+        Fahrenheit.new(50).to_celsius.should == 10
       end
 
       it "is a Temperature subclass" do
         Fahrenheit.new(0).should be_a(Temperature)
       end
     end
+  end
+
+  # Extra credit:
+  #
+  # 1. Fill in the tests below, so that they test for two class methods – ftoc and ctof
+  # 2. Refactor the rest of your code so it calls these two methods when appropriate
+  
+  describe "utility class methods" do
+
   end
 
 end
